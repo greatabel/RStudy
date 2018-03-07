54e1937f5dca123b159ba52253ed8a34f0b4d008
greatabel <myreceiver2for2github@gmail.com>
2018-03-07 11:14:56 +0800

    length in R seq

--- ch2Explore_R_Data/i2create_vector_by_rule.py
+++ ch2Explore_R_Data/i2create_vector_by_rule.py
@@ -1,2 +1,11 @@
-z <- seq(1, 5, by=0.5)
-z
+z0 <- seq(1, 5, by=0.5)
+z0
+z1 <- seq(from=1, to=5, by=0.5)
+z1
+
+z2 <- seq(10, 100, length=3)
+z2
+z2 <- seq(10, 100, length=4)
+z2
+z2 <- seq(10, 100, length=10)
+z2
