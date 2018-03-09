258a2565542e9f70d194fadb90aab9b374451560
greatabel <myreceiver2for2github@gmail.com>
2018-03-09 16:07:40 +0800

    element

--- ch2Explore_R_Data/i8matrix_create.R
+++ ch2Explore_R_Data/i8matrix_create.R
@@ -1,2 +1,14 @@
 m <- matrix(c(1,2,3,4,5,6), nrow =2 , ncol = 3)
 'matrix(c[1,2,3,4,5,6], nrow=2, ncol=3) =>'; m
+
+m <- matrix(c(1,2,3,4,5,6), nrow=2, ncol=4)
+m
+'-------'
+m <- matrix(c(1,2,3,4,5,6), nrow=2)
+m
+m <- matrix(c(1,2,3,4,5,6), ncol=3)
+m
+
+'element in matrix'
+'m[1,2]='; m[1, 2]
+'m[2,]='; m[2, ]
