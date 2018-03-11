a1b8b0066d56cd77c65ac897a267705d9955087d
greatabel <myreceiver2for2github@gmail.com>
2018-03-11 23:51:55 +0800

    i9matrix_use.R

--- ch2Explore_R_Data/i9matrix_use.R
+++ ch2Explore_R_Data/i9matrix_use.R
@@ -6,3 +6,13 @@ b
 a + b
 
 'c(1, 2, 3 ) + b='; c(1, 2, 3 ) + b
+
+z <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3)
+'z='; z
+'z * c(1, 20)='; z * c(1, 20)
+'z * c(1, 20, 300)='; z * c(1, 20, 300)
+
+m <- matrix(c(10, 20, 30, 40, 50, 60, 70, 80, 90), nrow=3)
+'m='; m
+'rowSums(m)=';rowSums(m)
+'m/rowSums(m)='; m/rowSums(m)
