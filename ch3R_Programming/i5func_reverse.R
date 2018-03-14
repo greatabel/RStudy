b9963301914dea3ca8e782737a21a9362d2f90b5
greatabel <myreceiver2for2github@gmail.com>
2018-03-14 22:30:13 +0800

    i5func_reverse.R

---
+++ ch3R_Programming/i5func_reverse.R
@@ -0,0 +1,12 @@
+reverse.list <- function(x){
+    for(i in 1: (length(x)/2)){
+        tmp = x[i]
+        x[i] = x[length(x)-i + 1]
+        x[length(x)-i+1] = tmp
+    }
+    return (x)
+}
+
+x <- c(1, 2, 3)
+x
+reverse.list(x)
