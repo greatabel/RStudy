7d39ef84c2c9f190b267027d7da4b195a624eea5
greatabel <myreceiver2for2github@gmail.com>
2018-03-15 17:15:56 +0800

    i2sample.R

--- ch4Probability_Statistics_Basic/i2sample.R
+++ ch4Probability_Statistics_Basic/i2sample.R
@@ -1,2 +1,10 @@
+library(crayon)
+
 '从52张扑克牌抽取5张'; sample(1: 52, 5) 
+
 '抛均匀硬币3次'; sample(c('H','T'), 3, replace=TRUE)
+
+'骰子丢10次'; sample(1:6, 10, replace=TRUE)
+
+cat(blue$bold('不等可能随机抽样，例如运动员投篮命中率0.8，投篮10次'))
+sample(c(1, 0), 10, replace=TRUE, prob=c(0.8, 0.2))
