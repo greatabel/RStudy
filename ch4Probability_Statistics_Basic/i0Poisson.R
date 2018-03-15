b77051125beba21d9a8e0e02fff073bb6f679c34
greatabel <myreceiver2for2github@gmail.com>
2018-03-15 17:04:51 +0800

    legend

--- ch4Probability_Statistics_Basic/i0Poisson.R
+++ ch4Probability_Statistics_Basic/i0Poisson.R
@@ -1,2 +1,10 @@
 curve(dexp(x, rate=1/2), from=0, to=5, ylim=c(0, 1.5),
     main="Exponential", col = "red")
+curve(dexp(x, rate=1), from=0, to=5, add=TRUE,
+    col = "blue")
+curve(dexp(x, rate=2), from=0, to=5, add=TRUE,
+     col = "green")
+
+text.legend = c("lambda = 0.5", "lambda = 1", "lambda = 2")
+legend("topright", legend = text.legend, lty = c(1, 1, 1),
+       col = c("red", "blue", "green"))
