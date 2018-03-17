c40b1f3e5cec7434ac294cef22db695bad4d27cf
greatabel <myreceiver2for2github@gmail.com>
2018-03-17 22:09:55 +0800

    i10matrix_with_barplot.R

---
+++ ch5Practical_statistical_graphics/i10matrix_with_barplot.R
@@ -0,0 +1,9 @@
+my.data <- matrix(c(38.1, 1.7,
+                    27.8, 28.7,
+                    34.1, 69.6)
+                , nrow=2)
+
+rownames(my.data) <- c("China", "Germany")
+colnames(my.data) <- c("primary", "second", "tertiary")
+
+my.data
