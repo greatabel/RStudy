c19b1abb09458bc93153db2a8f86466b93d4941f
greatabel <myreceiver2for2github@gmail.com>
2018-03-19 16:25:22 +0800

    i12spine.R

---
+++ ch5Practical_statistical_graphics/i12spine.R
@@ -0,0 +1,12 @@
+library(vcd)
+
+my.data <- matrix(c(38.1, 1.7,
+                    27.8, 28.7,
+                    34.1, 69.6)
+                , nrow=2)
+
+rownames(my.data) <- c("China", "Germany")
+colnames(my.data) <- c("primary", "second", "tertiary")
+
+
+spine(my.data, main = "Employment in 3 industries")
