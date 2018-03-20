ee22ea03ed64633c09dd1cbdd553483192975026
greatabel <myreceiver2for2github@gmail.com>
2018-03-20 16:43:39 +0800

    i0list.R

---
+++ ch7Advanced_Data_Structures/i0list.R
@@ -0,0 +1,11 @@
+goods <- list(name="Cookie", price=4.0, outdate=FALSE)
+goods
+cat('----')
+goods$name
+
+library(crayon)
+cat(red$bold$bgGreen("typeof"))
+
+typeof(goods$name)
+typeof(goods$price)
+typeof(goods$outdate)
