67160ec75866f7506b2e7680e2220aab3685431b
greatabel <myreceiver2for2github@gmail.com>
2018-03-20 16:35:29 +0800

    i1readline.R

--- ch6Data_IO/i0inner_dataset.R
+++ ch6Data_IO/i0inner_dataset.R
@@ -7,3 +7,12 @@ data[1:2,]
 library(crayon)
 cat(red$bold$bgGreen("mode(data) is "))
 mode(data)
+
+cat(blue$bold$bgGreen("names(data) is "))
+names(data)
+
+cat(yellow$bold$bgGreen("dim(data) is "))
+dim(data)
+
+cat(red$bold$bgGreen("data$lp100km is "))
+data$lp100km
---
+++ ch6Data_IO/i1readline.R
@@ -0,0 +1,4 @@
+data.fwf = read.fwf("i1data.txt", widths = c(7,7,7),
+            col.names=c("city", "latitude", "longitude"))
+
+data.fwf
