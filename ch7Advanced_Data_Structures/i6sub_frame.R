50e8c1a3d71936e1fbb5629feca7cf86bf29f3e4
greatabel <myreceiver2for2github@gmail.com>
2018-03-25 23:55:50 +0800

    i6sub_frame.R

---
+++ ch7Advanced_Data_Structures/i6sub_frame.R
@@ -0,0 +1,9 @@
+male <- c(124, 88, 200)
+female <- c(108, 56, 221)
+
+degree <- c("low", "middle", "high")
+myopia <- data.frame(degree, male, female)
+myopia
+'------'
+sub <- myopia[2:3, 1:2]
+sub
