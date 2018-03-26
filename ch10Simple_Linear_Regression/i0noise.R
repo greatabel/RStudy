8967c865d352ac88ee2e514d37eb5e7a37ebe42f
greatabel <myreceiver2for2github@gmail.com>
2018-03-27 01:27:48 +0800

    cluster

--- ch10Simple_Linear_Regression/i0noise.R
+++
@@ -1,5 +0,0 @@
-library(crayon)
-cat(red$bold$bgGreen("noise") , "\n")
-
-cars <- read.csv("i0racv.csv")
-cars
---
+++ ch13Cluster_Analysis/i0cluster.R
@@ -0,0 +1,20 @@
+library(crayon)
+
+
+cat(blue$bold("cluster"))
+
+countries = read.csv('i0countries_data.csv')
+'head(countries)='; head(countries)
+
+var = as.character(countries$countries)
+for(i in 1: 10) dimnames(countries)[[1]][i] = var[i]
+countries = countries[,2:3]
+names(countries) = c("Services(%)", "Aged_Population(%)")
+head(countries)
+
+my.km <- kmeans(countries, center = 2)
+my.km$center
+head(my.km$cluster)
+
+plot(countries, col = my.km$cluster)
+points(my.km$centers, col=1:2, pch =8, cex=2)
