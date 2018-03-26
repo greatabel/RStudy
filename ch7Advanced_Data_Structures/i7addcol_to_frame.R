68647fae44bf94559cf8e3b3e789c807bbfc416b
greatabel <myreceiver2for2github@gmail.com>
2018-03-26 11:36:06 +0800

    i7addcol_to_frame.R

--- ch7Advanced_Data_Structures/i7addcol_to_frame.R
+++ ch7Advanced_Data_Structures/i7addcol_to_frame.R
@@ -8,3 +8,11 @@ students <- data.frame(names, ages, stringsAsFactors=F)
 
 students
 
+cat(red$bold("rbind\n"))
+rbind(students, list('sariah', 15))
+
+cat(green$bold("cbind\n"))
+cbind(students, gender=c("M","F"))
+
+
+
