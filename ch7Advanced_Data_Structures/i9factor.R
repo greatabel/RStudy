342a9535d08e7b1e2925ea935812e8706b1064f8
greatabel <myreceiver2for2github@gmail.com>
2018-03-26 16:11:09 +0800

    i9factor.R set factor order

--- ch7Advanced_Data_Structures/i9factor.R
+++ ch7Advanced_Data_Structures/i9factor.R
@@ -9,3 +9,23 @@ sf <- factor(ssample)
 nsample <- c(2, 3, 3, 5, 5, 5, 6)
 nf <- factor(nsample)
 'nf='; nf
+
+cat(red$bold$bgGreen("查看内部结构"), '\n')
+str(nf)
+'unclass(nf)=';unclass(nf)
+cat(red$bold$bgGreen("--------"), '\n')
+str(sf)
+'unclass(sf)=';unclass(sf)
+
+
+cat(red$bold$bgGreen("指定顺序"), '\n')
+
+
+assessment <- c('weak', 'good', 'limited', 'fair')
+assessment0 <- factor(assessment)
+'assessment0='; assessment0
+
+assessment1 <- factor(assessment, order=TRUE,
+                levels=c('good', 'fair', 'limited','weak'))
+'assessment1='; assessment1
+str(assessment1)
