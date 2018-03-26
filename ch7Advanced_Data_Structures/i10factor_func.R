4586414a327391ca52c8ee1775b726acb197c1c3
greatabel <myreceiver2for2github@gmail.com>
2018-03-26 16:20:36 +0800

    i10factor_func.R split

--- ch7Advanced_Data_Structures/i10factor_func.R
+++ ch7Advanced_Data_Structures/i10factor_func.R
@@ -5,3 +5,12 @@ wt <- c(45, 46, 47, 48, 49, 100)
 group <- c("A", "B", "C","A", "B", "C")
 r = tapply(wt, as.factor(group), mean)
 r
+
+cat(red$bold$bgGreen("=========="), '\n')
+wt <- c(45, 46, 47, 48, 49, 100, 48, 69, 200)
+diet <- c("A", "B", "C","A", "B", "C", "A", "B", "C")
+gender <- c("M", "F", "M","M", "F", "M", "F", "M", "F")
+r = tapply(wt, list(as.factor(diet), as.factor(gender)), mean)
+r
+
+split(wt, list(diet, gender))
