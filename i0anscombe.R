eeb7780072ee4147a91c167275726010b2391ff3
greatabel <myreceiver2for2github@gmail.com>
2018-03-28 14:52:25 +0800

    i0anscombe.R chart

--- i0anscombe.R
+++ i0anscombe.R
@@ -1,5 +1,19 @@
+library(crayon)
+
+cat(blue$bold("anscombe"), '\n')
+
+
 data(anscombe)
 
 attach(anscombe)
 
 anscombe
+
+'cor(x1, y1)'; cor(x1, y1)
+'cor(x2, y2)'; cor(x2, y2)
+
+par(mfrow = c(2,2))
+plot(x1, y1, main = 'Plot 1')
+plot(x2, y2, main = 'Plot 2')
+plot(x3, y3, main = 'Plot 3')
+plot(x4, y4, main = 'Plot 4')
