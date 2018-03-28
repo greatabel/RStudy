842c038a3caa2ead42ae652a006feaf2a6b686bc
greatabel <myreceiver2for2github@gmail.com>
2018-03-28 15:23:35 +0800

    i1snake.R plot

--- 02Mastering Machine Learning with R/i1snake.R
+++ 02Mastering Machine Learning with R/i1snake.R
@@ -7,3 +7,13 @@ data(snake)
 
 'dim(snake)';  dim(snake)
 'head(snake)'; head(snake)
+
+'names(snake)'
+names(snake) <- c('content', 'yield')
+
+attach(snake)
+cat(blue$bold("after attach(snake)"), '\n')
+head(snake)
+
+plot(content, yield, xlab = 'water content of snow',
+    ylab = 'water yield')
