49f0ada43b0ef967c75fb828bc77434a9d129453
greatabel <myreceiver2for2github@gmail.com>
2018-03-28 16:26:36 +0800

    i3water.R

--- 02Mastering Machine Learning with R/i3water.R
+++ 02Mastering Machine Learning with R/i3water.R
@@ -7,3 +7,15 @@ library(alr3)
 data(water)
 'str(water)'; str(water)
 'head(water)'; head(water)
+
+cat(red$bold("-----------"), '\n')
+socal.water <- water[ ,-1]
+head(socal.water)
+
+library(corrplot)
+
+water.cor <- cor(socal.water)
+water.cor
+corrplot(water.cor, method = "ellipse")
+
+pairs(~ ., data = socal.water)
