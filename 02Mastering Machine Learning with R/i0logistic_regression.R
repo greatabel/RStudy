e62dc02c2a4aba38750f67d4f217c63d3b3f5e65
greatabel <myreceiver2for2github@gmail.com>
2018-03-29 15:48:26 +0800

    i0logistic_regression.R corrplot

--- 02Mastering Machine Learning with R/i0logistic_regression.R
+++ 02Mastering Machine Learning with R/i0logistic_regression.R
@@ -7,5 +7,29 @@ data(biopsy)
 cat(blue$bold$bgMagenta("str(biopsy)"), '\n')
 str(biopsy)
 
+'我们可以删除ID列'; biopsy$ID = NULL
 
+'重命名变量，并确认操作结果'
+names(biopsy) <- c("thick", "u.size", "u.shape",
+       "adhsn", "s.size", "nucl", "chrom", "n.nuc",
+           "mit", "class")
+names(biopsy)
+
+biopsy.v2 <- na.omit(biopsy)
+head(biopsy.v2)
+
+y <- ifelse(biopsy.v2$class == "malignant", 1, 0)
+
+library(reshape2)
+library(ggplot2)
+
+biop.m <- melt(biopsy.v2, id.var = "class")
+
+ggplot(data = biop.m, aes(x = class, y = value)) +
+geom_boxplot() + facet_wrap( ~ variable, ncol = 3)
+
+library(corrplot)
+bc <- cor(biopsy.v2[, 1:9])
+# 'bc'; bc
+corrplot.mixed(bc)
 
