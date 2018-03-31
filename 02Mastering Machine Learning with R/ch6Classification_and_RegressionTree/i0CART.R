e5f6d5135002b42e657565eaa2f75d60fc1efc39
greatabel <myreceiver2for2github@gmail.com>
2018-03-31 23:44:28 +0800

    i0CART.R

---
+++ 02Mastering Machine Learning with R/ch6Classification_and_RegressionTree/i0CART.R
@@ -0,0 +1,8 @@
+library(rpart) #classification and regression trees
+library(partykit) #treeplots
+library(MASS) #breast and pima indian data
+library(ElemStatLearn) #prostate data
+library(randomForest) #random forests
+library(xgboost) #gradient boosting 
+library(caret) #tune hyper-parameters
+
