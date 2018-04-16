a0b5de650e0c067eb9504a439142dc6b074ff8bb
greatabel <myreceiver2for2github@gmail.com>
2018-04-16 22:49:04 +0800

    NbClust

--- 02Mastering Machine Learning with R/ch8ClusterAnalysis/i0wine.R
+++ 02Mastering Machine Learning with R/ch8ClusterAnalysis/i0wine.R
@@ -17,3 +17,11 @@ str(df)
 table(wine$Class)
 
 #模型构建阶段
+numComplete <- NbClust(df, distance = "euclidean", 
+                       min.nc = 2, max.nc = 6, 
+                       method = "complete", index = "all")
+numComplete$Best.nc
+
+dis <- dist(df, method = "euclidean")
+hc <- hclust(dis, method = "complete")
+plot(hc, hang = -1,labels = FALSE, main = "Complete-Linkage")
