1837d830b23d0457f948144a020c1143e4cab94d
greatabel <myreceiver2for2github@gmail.com>
2018-04-16 22:47:42 +0800

    标准化

--- 02Mastering Machine Learning with R/ch8ClusterAnalysis/i0wine.R
+++ 02Mastering Machine Learning with R/ch8ClusterAnalysis/i0wine.R
@@ -11,3 +11,9 @@ names(wine) <- c("Class", "Alcohol", "MalicAcid", "Ash", "Alk_ash",
                  "magnesium", "T_phenols", "Flavanoids", "Non_flav",
                  "Proantho", "C_Intensity", "Hue", "OD280_315", "Proline")
 names(wine)
+
+df <- as.data.frame(scale(wine[, -1]))
+str(df)
+table(wine$Class)
+
+#模型构建阶段
