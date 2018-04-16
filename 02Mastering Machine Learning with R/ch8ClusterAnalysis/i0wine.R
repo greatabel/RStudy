ac962a8c3c1402af4c18a7c87fb02dd32f5502ca
greatabel <myreceiver2for2github@gmail.com>
2018-04-16 22:38:53 +0800

    i0wine.R

---
+++ 02Mastering Machine Learning with R/ch8ClusterAnalysis/i0wine.R
@@ -0,0 +1,8 @@
+library(cluster) #conduct cluster analysis
+library(compareGroups) #build descriptive statistic tables
+library(HDclassif) #contains the dataset
+library(NbClust) #cluster validity measures
+library(sparcl) #colored dendrogram
+
+data(wine)
+str(wine)
