fc6bfc5a73a1e5fe4e993a834331fec5f755521f
greatabel <myreceiver2for2github@gmail.com>
2018-04-18 23:32:25 +0800

    i1apriori_on_groceries.R

--- 02Mastering Machine Learning with R/ch10recommendation_engines/i0groceries.R
+++ 02Mastering Machine Learning with R/ch10recommendation_engines/i0groceries.R
@@ -9,3 +9,7 @@ cat(blue$bold$bgGreen("head(Groceries):"), '\n')
 head(Groceries)
 cat(blue$bold$bgGreen("str(Groceries):"), '\n')
 str(Groceries)
+
+itemFrequencyPlot(Groceries, topN = 10,type = "absolute")
+
+itemFrequencyPlot(Groceries, topN = 15)
---
+++ 02Mastering Machine Learning with R/ch10recommendation_engines/i1apriori_on_groceries.R
@@ -0,0 +1,18 @@
+library(arules)
+library(arulesViz)
+
+library(crayon)
+
+data(Groceries)
+'设定最小支持度为0.001 最小置信度90% 项集中最大项数为4'
+rules <- apriori(Groceries, 
+                 parameter = list(supp = 0.001, conf = 0.9, maxlen = 4))
+
+cat(blue$bold$bgGreen("rules:"), '\n')
+rules
+
+rules <- sort(rules, by = "lift", decreasing = TRUE)
+inspect(rules[1:5])
+cat(red$bold$bgGreen("======="), '\n')
+rules <- sort(rules, by = "confidence", decreasing = TRUE)
+inspect(rules[1:5])
