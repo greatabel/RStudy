93ff03e5d217c0b57dc622cf4e3f5c0c90337baa
greatabel <myreceiver2for2github@gmail.com>
2018-04-18 23:58:11 +0800

    i4recommend_SVD.R

---
+++ 02Mastering Machine Learning with R/ch10recommendation_engines/i4recommend_SVD.R
@@ -0,0 +1,8 @@
+library(crayon)
+cat(blue$bold$bgGreen("-----------------"), '\n')
+
+ratings <- c(3,5,5,5,1,1,5,2,5,1,1,5,3,5,1,5,4,2,4,3,4,2,1,4)
+ratingMat <- matrix(ratings, nrow = 6)
+rownames(ratingMat) <- c("Homer","Marge","Bart","Lisa","Flanders","Me")
+colnames(ratingMat) <- c("Avengers","American Sniper","Les Miserable","Mad Max")
+ratingMat
