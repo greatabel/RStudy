7eec9d972a9ec4a4d69f292199ef12c47fe879c7
greatabel <myreceiver2for2github@gmail.com>
2018-04-19 14:05:36 +0800

    i5die_func.R

--- 03Hands-On Programming with R/ch1R_basics/i3func.R
+++ 03Hands-On Programming with R/ch1R_basics/i3func.R
@@ -5,3 +5,12 @@ cat(blue$bold$bgMagenta("R函数执行方式是从内到外"), '\n')
 '阶乘'; factorial(5)
 
 'round(mean(1:6))'; round(mean(1:6))
+
+cat(blue$bold$bgMagenta("🎲"), '\n')
+die <- 1: 6
+sample(x = die, size = 1)
+sample(x = die, size = 1)
+
+'查看参数名'; args(sample); args(round)
+round(3.14159265359, digits = 4)
+sample(size = 1, x = die)
---
+++ 03Hands-On Programming with R/ch1R_basics/i5die_func.R
@@ -0,0 +1,10 @@
+library(crayon)
+
+cat(black$bold$bgMagenta("R函数就是一种不同类型的R对象
+     它们保护的不是数据，而是代码"), '\n')
+
+die <- 1:6
+dice <- sample(die , size = 2, replace = TRUE)
+dice
+sum(dice)
+
