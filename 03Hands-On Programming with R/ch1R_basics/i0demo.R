5e1b5000af28a5df3057d16f5ba220c8db1e25c2
greatabel <myreceiver2for2github@gmail.com>
2018-04-19 13:27:07 +0800

    i0demo.R ls()

--- 03Hands-On Programming with R/ch1R_basics/i0demo.R
+++ 03Hands-On Programming with R/ch1R_basics/i0demo.R
@@ -1,6 +1,21 @@
 library(crayon)
 
+
 cat(blue$bold$bgMagenta("1 + 1"), '\n')
 1 + 1
+
 cat(blue$bold$bgMagenta("100:130"), '\n')
 100:130
+
+a <- 1
+'a='; a
+'a + 2 ='; a + 2
+
+'R区分大小写'
+Name <- 1
+name <- 0
+'Name='; Name
+'name='; name
+
+'查看已经命名啦哪些R对象'
+'ls()='; ls()
