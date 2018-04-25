fdb124834b228b005885c53f4b0dd467b4ef2751
greatabel <myreceiver2for2github@gmail.com>
2018-04-25 16:01:11 +0800

    i3dim.R class typeof str

--- 03Hands-On Programming with R/ch3R_objects/i13frame.R
+++ 03Hands-On Programming with R/ch3R_objects/i13frame.R
@@ -2,6 +2,14 @@ library(crayon)
 
 cat(black$bold$bgCyan("数据框frame"), '\n')
 
-df <- data.frame(face=c('ace','two','six'), suit=c('clubs','clubs', 'clubs'), 
+df <- data.frame(face=c('ace','two','six'), 
+                 suit=c('clubs','clubs', 'clubs'), 
                  value=c(1,2, 3))
 df
+
+cat(black$bold$bgCyan("typeof(df)"), '\n')
+typeof(df)
+cat(black$bold$bgCyan("class(df)"), '\n')
+class(df)
+cat(black$bold$bgGreen("str(df)"), '\n')
+str(df)
