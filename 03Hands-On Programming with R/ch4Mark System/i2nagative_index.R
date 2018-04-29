5c61afa1392c0ba8e3849c6bb0f1f1320a1d8543
greatabel <myreceiver2for2github@gmail.com>
2018-04-29 18:26:36 +0800

    i2nagative_index.R

--- 03Hands-On Programming with R/ch4Mark System/i2nagative_index.R
+++ 03Hands-On Programming with R/ch4Mark System/i2nagative_index.R
@@ -7,3 +7,6 @@ cat(black$bold$bgCyan("'返回除第一行元素之外的'"), '\n')
 deck[-1, 1:3]
 cat(black$bold$bgCyan("'返回排除第2行到第52行'"), '\n')
 deck[-(2:52), 1:3]
+
+cat(black$bold$bgCyan("空格告诉R提取代表维度所有元素"), '\n')
+deck[1, ]
