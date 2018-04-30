bd0d675eda16c91b0e1afe1e1ab41ac709a7795c
greatabel <myreceiver2for2github@gmail.com>
2018-04-30 08:40:41 +0800

    name index

--- 03Hands-On Programming with R/ch4Mark System/i2nagative_index.R
+++ 03Hands-On Programming with R/ch4Mark System/i2nagative_index.R
@@ -10,3 +10,13 @@ deck[-(2:52), 1:3]
 
 cat(black$bold$bgCyan("空格告诉R提取代表维度所有元素"), '\n')
 deck[1, ]
+
+cat(black$bold$bgCyan("R会匹配索引值为TRUE的行位置/列位置"), '\n')
+deck[1, c(TRUE, TRUE, FALSE)]
+
+
+cat(red$bold$bgCyan("如果被索引的对象有名称，可以采用名称索引"), '\n')
+deck[1, c('face', 'suit', 'value')]
+
+'整个value列'
+deck[, 'value']
