94157b149377eebadfc166b66f47a6aa3299d72e
greatabel <myreceiver2for2github@gmail.com>
2018-05-02 11:46:32 +0800

    i2boolean_operator.R

--- 03Hands-On Programming with R/ch5Change_RObject_Value/i2boolean_operator.R
+++ 03Hands-On Programming with R/ch5Change_RObject_Value/i2boolean_operator.R
@@ -6,3 +6,13 @@ deck <- read.csv(file="../data/cards.csv", header=TRUE, sep=",")
 deck3 <- deck
 deck3$value <- 0
 deck3$value['hearts' == deck3$suit] <- 1
+
+cat(red$bold$bgCyan("Q牌"), '\n')
+deck3[deck3$face == 'queen', ]
+
+cat(red$bold$bgCyan("黑桃"), '\n')
+deck3[deck3$suit == 'spades', ]
+
+cat(red$bold$bgCyan("黑桃Q 点数设置为13"), '\n')
+deck3$value[deck3$face == 'queen' & deck3$suit == 'spades'] <- 13
+deck3[deck3$face == 'queen' & deck3$suit == 'spades',]
