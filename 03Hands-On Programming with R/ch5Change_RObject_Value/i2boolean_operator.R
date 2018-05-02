ce91b892138a39b914ee8672de73a8cfc27f77db
greatabel <myreceiver2for2github@gmail.com>
2018-05-02 11:54:17 +0800

    i2boolean_operator.R excercise

--- 03Hands-On Programming with R/ch5Change_RObject_Value/i2boolean_operator.R
+++ 03Hands-On Programming with R/ch5Change_RObject_Value/i2boolean_operator.R
@@ -16,3 +16,18 @@ deck3[deck3$suit == 'spades', ]
 cat(red$bold$bgCyan("黑桃Q 点数设置为13"), '\n')
 deck3$value[deck3$face == 'queen' & deck3$suit == 'spades'] <- 13
 deck3[deck3$face == 'queen' & deck3$suit == 'spades',]
+
+
+cat(red$bold$bgCyan("###练习###"), '\n')
+for(w in -5:5) {
+    cat(w, w > 0,'\n')
+}
+for(x in 5:15){
+    cat(x, 10 < x & x < 20, '\n')
+}
+y <- 'February'
+print(y == 'February')
+
+z <- c('Monday', 'Tuesday', 'Friday')
+print(all(z %in% c('Monday', 'Tuesday', 'Wednesday',
+    'Thursday', 'Friday', 'Saturday', 'Sunday')))
