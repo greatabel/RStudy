5f6445aaa46a0f7b304658d3331391fda5a4a170
greatabel <myreceiver2for2github@gmail.com>
2018-05-02 17:19:55 +0800

    i0env.R

---
+++ 03Hands-On Programming with R/ch6Env_System/i0env.R
@@ -0,0 +1,15 @@
+library(crayon)
+library(pryr)
+
+cat(red$bold$bgGreen("环境"), '\n')
+
+parenvs(all = TRUE)
+
+cat(red$bold$bgGreen("as.environment('package:stats')"), '\n')
+as.environment('package:stats')
+
+cat(red$bold$bgGreen("parent.env(globalenv())"), '\n')
+parent.env(globalenv())
+
+cat(red$bold$bgGreen("ls(emptyenv())"), '\n')
+ls(emptyenv())
