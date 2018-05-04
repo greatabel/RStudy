df5afccf538ddeb7eea1f16f0988017782dc7b3b
greatabel <myreceiver2for2github@gmail.com>
2018-05-04 11:19:28 +0800

    i4score_func.R

---
+++ 03Hands-On Programming with R/ch7Program/i4score_func.R
@@ -0,0 +1,55 @@
+library(crayon)
+cat(red$bold$bgGreen("函数化"), '\n')
+
+
+get_symbols <- function() {
+    wheel <- c('DD', '7', 'BBB', 'BB', 'B', 'C', '0')
+    sample(wheel, size = 3, replace = TRUE,
+        prob = c(0.03, 0.03, 0.06, 0.1, 0.25, 0.01, 0.52))
+}
+
+
+score <- function(symbols){
+
+    # 识别情形
+    same <- symbols[1] == symbols[2] && symbols[2] == symbols[3]
+    bars <- symbols %in% c('B', 'BB', 'BBB')
+
+    # 计算中奖金额
+    if (same) {
+        payouts <- c("DD" = 100,
+                 "7" = 80,
+                 "BBB" = 40,
+                 "BB" = 25,
+                 "B" = 10,
+                 "C" = 10,
+                 "0" = 0
+                 )
+        prize <- unname(payouts[symbols[1]])
+
+    } else if (all(bars)) {
+        prize <- 5
+
+    } else {
+        cherries <- sum(symbols == 'C')
+        prize <- c(0, 2, 5)[cherries + 1]
+    }
+    # 根据钻石的个数调整中奖金额
+    diamonds <- sum(symbols == 'DD')
+    prize * 2 ^ diamonds
+
+}
+
+
+play <- function() {
+    symbols <- get_symbols()
+    print(symbols)
+    score(symbols)
+}
+
+
+play()
+cat(red$bold$bgGreen("play again!"), '\n')
+play()
+cat(red$bold$bgGreen("play again!"), '\n')
+play()
