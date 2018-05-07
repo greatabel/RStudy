45e23d9fa66e3054101d14fd9f5b07f0fb05ebf6
greatabel <myreceiver2for2github@gmail.com>
2018-05-07 23:30:49 +0800

    i0abs_demo.R

---
+++ 03Hands-On Programming with R/ch10Speed_up/i0abs_demo.R
@@ -0,0 +1,22 @@
+library(crayon)
+
+abs_loop <- function(vec) {
+    for (i in 1: length(vec)){
+        if ( vec[i] < 0) {
+            vec[i] <- -vec[i]
+        }
+    }
+    vec
+}
+
+a <- c(1:100)
+abs_loop(a)
+
+cat(red$bold$bgGreen("向量化版本"), '\n')
+
+abs_set <- function(vec) {
+    negs <-  vec < 0
+    vec[negs] <- vec[negs] * -1
+    vec
+}
+abs_set(a)
