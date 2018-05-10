895191ddc13adc47b2559b313f5f34cf9c4585ba
greatabel <myreceiver2for2github@gmail.com>
2018-05-10 11:18:00 +0800

    i1change_symbols.R change_symbols

--- 03Hands-On Programming with R/ch10Speed_up/i1change_symbols.R
+++ 03Hands-On Programming with R/ch10Speed_up/i1change_symbols.R
@@ -1,20 +1,23 @@
 change_symbols <- function(vec) {
     for(i in 1: length(vec)){
         if (vec[i] == 'DD') {
-            vec
+            vec[i] <- 'joker'
          } else if (vec[i] == 'C') {
-
+            vec[i] <- 'ace'
          } else if (vec[i] == '7') {
-            
+            vec[i] <- 'king'
          } else if (vec[i] == 'B') {
-            
+            vec[i] <- 'queen'
          } else if (vec[i] == 'BB') {
-            
+            vec[i] <- 'jack'
          } else if (vec[i] == 'BBB') {
-            
+            vec[i] <- 'ten'
          } else {
-
+            vec[i] <- 'nine'
          }
     }
     vec
 }
+
+vec <- c('DD', 'C', '7', 'B', 'BB', 'BBB', '0')
+change_symbols(vec)
